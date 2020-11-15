bfb9778d93abcdd700dec21b2eb8f1237170dc65
Jed <jed@mijar.es>
2020-11-15 13:33:32 -0600

    functional draft

--- TCPServer.py
+++ TCPServer.py
@@ -2,36 +2,6 @@ from socket import *
 import threading
 import sys
 
-class Sender(threading.Thread):
-    def __init__(self, socket, name):
-        super().__init__()
-        self.socket = socket
-        self.name = name
-
-    def run(self):
-        while True:
-            # print('{}: 'send.format(self.name), end='')
-            sys.stdout.flush()
-            message = sys.stdin.readline()[:-1]
-            self.socket.sendall('{}: {}'.format(self.name, message).encode())
-
-class Receiver(threading.Thread):
-    def __init__(self, socket, name):
-        super().__init__()
-        self.socket = socket
-        self.name = name
-    
-    def run(self):
-        while True:
-            message = self.socket.recv(1024).decode()
-            if message:
-                print('\r{}\n{}: '.format(message, self.name), end = '')
-                print("test")
-            else:
-                print('\nLost connection to the server.')
-                self.socket.close()
-                os._exit(0)
-
 answer = input("Will you host? ")
 
 if(answer == 'y'):
@@ -49,14 +19,15 @@ if(answer == 'y'):
         def run(self):
             while True:
                 message = None
-                message = (self.socket.recv(1024)).decode()
+                message = (self.socket.recv(1024)).decode('ascii')
                 if message:
                     for client in activeConnections:
-                        socket.sendall(message.encode())
+                        if client != self:
+                            client.socket.sendall(message.encode('ascii'))
 
     # create TCP welcoming socket
     serverSocket = socket(AF_INET,SOCK_STREAM)
-    # serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serverSocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
     serverSocket.bind(("",serverPort))
 
     # server begins listening for incoming TCP requests
@@ -74,7 +45,7 @@ if(answer == 'y'):
         activeConnections.append(newConnection)
 
     #     # read a sentence of bytes from socket sent by the client
-    #     sentence = connectionSocket.recv(1024).decode()
+    #     sentence = connectionSocket.recv(1024).decode('ascii')
 
     #     # output to console the sentence received from the client
     #     print (sentence)
@@ -86,7 +57,7 @@ if(answer == 'y'):
     #     # capitalizedSentence = sentence.upper()
         
     #     # send back modified sentence over the TCP connection
-    #     connectionSocket.send((username + ': ' + capitalizedSentence).encode())
+    #     connectionSocket.send((username + ': ' + capitalizedSentence).encode('ascii'))
 
     #     # output to console the sentence sent back to the client
     #     print (username + ':' + capitalizedSentence)
@@ -94,6 +65,37 @@ if(answer == 'y'):
     #     # close the TCP connection; the welcoming socket continues
     # connectionSocket.close()
 else: # client
+
+    class Sender(threading.Thread):
+        def __init__(self, socket, name):
+            super().__init__()
+            self.socket = socket
+            self.name = name
+
+        def run(self):
+            while True:
+                print('{}: '.format(self.name), end='')
+                sys.stdout.flush()
+                message = sys.stdin.readline()[:-1]
+                self.socket.sendall('{}: {}'.format(self.name, message).encode('ascii'))
+
+    class Receiver(threading.Thread):
+        def __init__(self, socket, name):
+            super().__init__()
+            self.socket = socket
+            self.name = name
+        
+        def run(self):
+            while True:
+                message = self.socket.recv(1024).decode('ascii')
+                if message:
+                    print('\r{}\n{}: '.format(message, self.name), end = '')
+                    # print("test")
+                else:
+                    print('\nLost connection to the server.')
+                    self.socket.close()
+                    os._exit(0)
+
     # serverName = '172.17.255.255'
     # serverName = '172.17.0.1'
     serverName = 'localhost' # Jed IP
@@ -122,13 +124,13 @@ else: # client
     #     sentence = input("")
 
     #     # send the user's line over the TCP connection
-    #     clientSocket.send((username + ': ' + sentence).encode())
+    #     clientSocket.send((username + ': ' + sentence).encode('ascii'))
 
     #     #output to console what is sent to the server
     #     print (username + ': ' + sentence)
 
     #     # get user's line back from server having been modified by the server
-    #     modifiedSentence = clientSocket.recv(1024).decode()
+    #     modifiedSentence = clientSocket.recv(1024).decode('ascii')
 
     #     # output the modified user's line
     #     print (modifiedSentence)
